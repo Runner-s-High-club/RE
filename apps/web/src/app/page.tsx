7adb6a95fd1e7b39a3c373ecbdc090dd6d0712d1
junghaeseok <haeru9410@gmail.com>
2024-01-21 18:04:29 +0900

    [UPDATE] yarn workspace -> pnpm workspace 전환 작업

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import Image from 'next/image';
+import { Button } from '@fe/ui';
+
+export default function Home() {
+  return (
+    <main>
+      <Button size='large' label='버튼' />
+    </main>
+  );
+}
