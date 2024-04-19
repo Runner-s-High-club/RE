821048d4182faaa9d5e597a70258b9a4c2a02329
junghaeseok <haeru9410@gmail.com>
2024-04-19 20:45:05 +0900

    [UPDATE] 로그인 로직 임시 구현
    - 추후 웹뷰 작업으로 로직 완성

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,5 +1,40 @@
 'use client';
 
+import { styled } from 'styled-components';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <AgreeSection>
+      <ContentBox>
+        <AgreeTitle>메인화면</AgreeTitle>
+      </ContentBox>
+    </AgreeSection>
+  );
 }
+
+const AgreeSection = styled.section`
+  width: 100%;
+  height: auto;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-width: 320px;
+`;
+
+const ContentBox = styled.div`
+  width: 60%;
+  height: 100%;
+  min-width: 300px;
+  max-width: 760px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const AgreeTitle = styled.h2`
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.primary};
+  user-select: none;
+  margin-top: 40%;
+`;
